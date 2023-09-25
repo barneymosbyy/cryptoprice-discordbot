dd3007535c49870b60c553894a3789723d337e00
Maker <petr.stencar@gmail.com>
2023-09-25 20:22:06 +0200

    added daily change % and change role color based on positive / negative day

--- main.py
+++ main.py
@@ -1,26 +1,66 @@
 import discord
+import os
+import datetime
+from pytz import timezone
+from dotenv import load_dotenv
 from random import randint
-from pyth import get_price, asyncio
+from pyth import get_price, get_price_change, asyncio
 
-TOKEN = "YOUR SECRET TOKEN"
+load_dotenv()
+DISCORD_API_KEY = os.getenv('DISCORD_API_KEY')
 SYMBOL = "SOL/USD"
+GREEN = 0x229e8b
+GREEN_COLOR_VALUE = 2268811
+RED = 0xc92a35
+RED_COLOR_VALUE = 13183541
+DEFAULT_COLOR_VALUE = 0
 
 status = discord.Status.online
 intents = discord.Intents.default()
 client = discord.Client(intents=intents, status=status)
+previous_day = None
 
 
 async def change_nickname():
+    global previous_day
     while True:
         try:
             price = await get_price()
+            cet_timezone = timezone('UTC')
+            current_date = datetime.datetime.now(cet_timezone).date()
+            if current_date != previous_day:
+                daily_close_price = await get_price_change()
+                previous_day = current_date
+            change_percentage = (price - daily_close_price) / daily_close_price * 100
             for guild in client.guilds:
                 try:
-                    await asyncio.sleep(2) # Allows some time for the client to update its cache with the latest information
-                    await guild.me.edit(nick=f"${price:.2f}")
-                except discord.errors.Forbidden: # Missing Change Nickname permission
+                    await guild.me.edit(nick=f"{price:.2f} ({change_percentage:.2f}%)")
+                    all_bot_roles = [role.name for role in guild.me.roles]
+                    all_bot_roles.remove("@everyone")
+                    all_bot_roles.reverse()
+                    # print(f"{guild.name}: {all_bot_roles}")
+                    try:
+                        role = discord.utils.get(guild.roles, name=all_bot_roles[1])
+                    except IndexError:
+                        # print(f"Missing the 2 role setup to change role color in {guild.name}")
+                        continue
+                    if role:
+                        if change_percentage > 0 and role.color.value != GREEN_COLOR_VALUE:
+                            await role.edit(color=discord.Color(GREEN))
+                            # print(f"Changing to green in {guild.name}")
+                        elif change_percentage < 0 and role.color.value != RED_COLOR_VALUE:
+                            await role.edit(color=discord.Color(RED))
+                            # print(f"Changing to red in {guild.name}")
+                        elif change_percentage == 0 and role.color.value != DEFAULT_COLOR_VALUE:
+                            await role.edit(color=discord.Color(0x000000))
+                            # print(f"Changing to default in {guild.name}")
+                    else:
+                        print("Role missing")
+                except discord.errors.Forbidden:
+                    print("Missing permissions")
                     continue
-                except discord.errors.NotFound: # Guild not found or bot is not a member of the guild
+                except discord.errors.NotFound:  # Guild not found or bot is not a member of the guild
+                    print("Guild not found")
                     continue
                 except OSError as e:
                     print(f"Error occurred: {e}. Retrying...")
@@ -40,4 +80,4 @@ async def on_ready():
     await client.change_presence(activity=activity)
     client.loop.create_task(change_nickname())
 
-client.run(TOKEN)
+client.run(DISCORD_API_KEY)
--- pyth.py
+++ pyth.py
@@ -1,30 +1,61 @@
 import asyncio
+import requests
+import datetime
+from pytz import timezone
 import pythclient.exceptions
 from pythclient.pythaccounts import PythPriceAccount, PythPriceStatus
 from pythclient.solana import SolanaClient, SolanaPublicKey, PYTHNET_HTTP_ENDPOINT, PYTHNET_WS_ENDPOINT
 
+PYTH_EVM_PRICE_ID = "0xef0d8b6fda2ceba41da15d4095d1da392a0d2f8ed0c6c7bc0f4cfac8c280b56d"
+SOLANA_PRICE_ID = "H6ARHf6YXhGYeQfUzQNGk6rDNnLBQKrenN712K4AQJEG"
+
 
 async def get_price():
     # pythnet SOL/USD price account key (available on pyth.network website)
-    account_key = SolanaPublicKey("H6ARHf6YXhGYeQfUzQNGk6rDNnLBQKrenN712K4AQJEG")
+    account_key = SolanaPublicKey(SOLANA_PRICE_ID)
     solana_client = SolanaClient(endpoint=PYTHNET_HTTP_ENDPOINT, ws_endpoint=PYTHNET_WS_ENDPOINT)
     price: PythPriceAccount = PythPriceAccount(account_key, solana_client)
 
+    try:
+        await price.update()
+        price_status = price.aggregate_price_status
+        if price_status == PythPriceStatus.TRADING:
+            solprice = price.aggregate_price
+            return solprice
+        else:
+            print("Price is not valid now. Status is", price_status)
+    except pythclient.exceptions.SolanaException:
+        print("pythclient.exceptions.SolanaException. Waiting 30 seconds and trying again.")
+        await asyncio.sleep(30)
+        await solana_client.close()
+        return await get_price()
+    except RuntimeError as e:
+        print("RuntimeError:", e)
+        await asyncio.sleep(30)
+        await solana_client.close()
+        return await get_price()  # Recreate the session and retry the request
+    except Exception as e:
+        print("Error:", e)
+        await asyncio.sleep(30)
+        await solana_client.close()
+        return await get_price()
+    finally:
+        await solana_client.close()
+
+
+async def get_price_change():
+    cet_timezone = timezone('UTC')
+    current_datetime = datetime.datetime.now(cet_timezone)
+    daily_close = current_datetime.replace(hour=0, minute=0, second=0, microsecond=0)
+    daily_close_unix = int(daily_close.timestamp())
+
     while True:
-        try:
-            await price.update()
-            price_status = price.aggregate_price_status
-            if price_status == PythPriceStatus.TRADING:
-                solprice = price.aggregate_price
-                return solprice
-            else:
-                print("Price is not valid now. Status is", price_status)
-        except pythclient.exceptions.SolanaException:
-            print("pythclient.exceptions.SolanaException. Waiting 60 seconds and trying again.")
-            await asyncio.sleep(60)
-        except Exception as e:
-            # Handle other exceptions
-            print("Error:", e)
-            await asyncio.sleep(60)
-        finally:
-            await solana_client.close()
+        price_data = requests.get(f"https://hermes.pyth.network/api/get_price_feed?id={PYTH_EVM_PRICE_ID}&publish_time={daily_close_unix}")
+
+        if price_data.status_code == 200:
+            price_data_json = price_data.json()
+            daily_close_price = int(price_data_json["price"]["price"]) / 1e8
+            return daily_close_price
+        else:
+            print(f"Request failed with status code: {price_data.status_code}")
+            await asyncio.sleep(5)
